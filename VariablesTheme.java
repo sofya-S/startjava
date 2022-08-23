c4f3dfcfbf92269324b953eacba9084f937340f2
sofya-S <sofyas@brainway.co.il>
2022-08-23 13:55:45 +0300

    Update VariablesTheme.java
    homework correction

--- VariablesTheme.java
+++ VariablesTheme.java
@@ -19,10 +19,10 @@ public class VariablesTheme {
         System.out.println("boolean - is task1 complete " + task);
 
         System.out.println("\n2. Cost calculation for discount items");
-        float penPrice = 100f;
-        float bookPrice = 200f;
-        float discount = (penPrice * 11) / 100 + (bookPrice * 11) / 100;
-        float discountPrice = (penPrice + bookPrice) - discount;
+        double penPrice = 100f;
+        double bookPrice = 200f;
+        double discount = (penPrice + bookPrice) * 0.11;
+        double discountPrice = (penPrice + bookPrice) - discount;
         System.out.println("Discount amount: " + discount);
         System.out.println("Total amount after discount: " + discountPrice);
 
@@ -34,20 +34,14 @@ public class VariablesTheme {
 
         System.out.println("\n4. Display min and max values for numeric data types");
         byte byteVal = Byte.MAX_VALUE;
-        System.out.print("initial value for byte - " + byteVal + ", incremented value for byte - ");
-        System.out.print(byteVal+1);
-        System.out.print(", decremented value for short - ");
-        System.out.println(byteVal-1);
+        System.out.println("initial value for byte - " + byteVal + ", incremented value for byte - " + (byteVal++)
+         + ", decremented value for byte - " + (byteVal--));
         short shortVal = Short.MAX_VALUE;
-        System.out.print("initial value for short - " + shortVal + ", incremented value for short - ");
-        System.out.print(shortVal+1);
-        System.out.print(", decremented value for short - ");
-        System.out.println(shortVal-1);
+        System.out.println("initial value for short - " + shortVal + ", incremented value for short - " + (shortVal++)
+         + ", decremented value for short - " + (shortVal--));
         long longVal = Long.MAX_VALUE;
-        System.out.print("initial value for long - " + longVal + ", incremented value for long - ");
-        System.out.print(longVal + 1);
-        System.out.print(", decremented value for long - ");
-        System.out.println(longVal-1);
+        System.out.println("initial value for long - " + longVal + ", incremented value for long - " + (longVal ++)
+         + ", decremented value for long - " + (longVal--));
 
         System.out.println("\n5. Swapping variable values");
         int x = 2;
@@ -59,27 +53,27 @@ public class VariablesTheme {
         System.out.println(". New values after exchange: first variable = " + x + ", second variable = " + y);
         System.out.print("Part II, math. Initial values: first variable = " + x + ", second variable = " + y);
         x += y;
-        y -= x;
+        y = x - y;
         x -= y;
         System.out.println(". New values after exchange: first variable = " + x + ", second variable = " + y);
         System.out.print("Part III, bitwise operation. Initial values: first variable = "
                 + x + ", second variable = " + y);
-        x = x ^ y;
-        y = x ^ y;
-        x = x ^ y;
+        x ^= y;
+        y ^= x;
+        x ^= y;
         System.out.println(". New values after exchange: first variable = " + x + ", second variable = " + y);
 
-        System.out.println("\n6. Console output of sybmols and their codes");
+        System.out.println("\n6. Console output of symbols and their codes");
         char charA = 35;
         char charB = 38;
         char charC = 64;
         char charD = 94;
         char charE = 95;
-        System.out.println((int)charA + " - " + charA);
-        System.out.println((int)charB + " - " + charB);
-        System.out.println((int)charC + " - " + charC);
-        System.out.println((int)charD + " - " + charD);
-        System.out.println((int)charE + " - " + charE);
+        System.out.println((int) charA + " - " + charA);
+        System.out.println((int) charB + " - " + charB);
+        System.out.println((int) charC + " - " + charC);
+        System.out.println((int) charD + " - " + charD);
+        System.out.println((int) charE + " - " + charE);
 
         System.out.println("\n7. Display amount of hundreds, tens and ones in a number");
         int num = 123;
@@ -97,18 +91,18 @@ public class VariablesTheme {
         char rightHook = 40;
         char leftHook = 41;
         char space = 32;
-        System.out.println(space+""+space+""+space+""+space+""+space+""+slash+""+backSlash);
-        System.out.println(space+""+space+""+space+space+""+slash+""+space+""+space+""+backSlash);
-        System.out.println(space+""+space+""+space+slash+""+underscore+""+rightHook+""+space+""+leftHook+""+backSlash);
-        System.out.println(space+""+space+""+slash+space+""+space+space+""+space+""+space+""+space+""+backSlash);
-        System.out.println(space+""+slash+""+underscore+""+underscore+""+underscore+""+underscore+""+slash+""+
-                backSlash+""+underscore+""+underscore+""+backSlash);
+        System.out.println("" + space + space + space + space + space + slash + backSlash);
+        System.out.println("" + space + space + space + space + slash + space + space + backSlash);
+        System.out.println("" + space + space + space + slash + underscore + rightHook + space + leftHook + backSlash);
+        System.out.println("" + space + space + slash + space + space + space + space + space + space + backSlash);
+        System.out.println("" + space + slash + underscore + underscore + underscore + underscore + slash + 
+                backSlash + underscore + underscore + backSlash);
 
         System.out.println("\n9. Product and sum of digits");
         int numOriginal = 345;
         int numHundred = numOriginal / 100;
         int numTen = numOriginal / 10 % 10;
-        int numOne = numOriginal % 100 % 10;
+        int numOne = numOriginal % 10;
         int sumDigits = numHundred + numTen + numOne;
         int prodDigits = numHundred * numTen * numOne;
         System.out.println("Sum of digits for " + numOriginal + " = " + sumDigits);
@@ -119,6 +113,6 @@ public class VariablesTheme {
         int hour = totalSeconds / 3600 ;
         int minute = (totalSeconds % 3600) / 60;
         int second = totalSeconds  % 60;
-        System.out.println(hour +":" + minute + ":" + second);
+        System.out.println(hour + ":" + minute + ":" + second);
     }
 }
