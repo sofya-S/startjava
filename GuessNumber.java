a3a37eedecc60ac24f5ee8e1bbf4f2ac59923943
sofya-S <110479867+sofya-S@users.noreply.github.com>
2022-10-14 18:09:22 +0300

    Add files via upload

---
+++ GuessNumber.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class GuessNumber{
+
+        	public static void main(String[] args) {
+            int random = (int) (Math.random() * 100 + 1);
+            Scanner sc = new Scanner(System.in);
+            while(true) {
+                System.out.print("Enter your guess (1 to 100): ");
+                int guess = sc.nextInt();
+                if(guess > random){
+                    System.out.println("Your guess is too big");
+                } else if(guess < random) {
+                    System.out.println("Your guess is too small");
+                } else {
+                    break;
+                }
+            } 
+            System.out.println("You win!");       
+        }
+}
