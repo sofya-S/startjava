cc355011025cd852d4c40c26231440c217fa6fe8
sofya-S <110479867+sofya-S@users.noreply.github.com>
2022-10-17 16:02:54 +0300

    Update GuessNumber.java

--- GuessNumber.java
+++ GuessNumber.java
@@ -8,13 +8,13 @@ public class GuessNumber {
         System.out.print("Enter your guess (1 to 100): ");
         int guess = sc.nextInt();
         while(random != guess) {
-            System.out.print("Enter your guess (1 to 100): ");
-            guess = sc.nextInt();
             if(guess > random) {
                 System.out.println("Your guess is too big");
             } else if(guess < random) {
                 System.out.println("Your guess is too small");
             }
+            System.out.print("Enter your guess (1 to 100): ");
+            guess = sc.nextInt();            
         } 
         System.out.println("You win!");       
     }
