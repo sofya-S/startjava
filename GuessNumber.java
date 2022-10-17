7c008ec5b41912211b5abfd31844038eb9f913ec
sofya-S <sofyas@brainway.co.il>
2022-10-17 15:57:47 +0300

    compare variabes for cycle break

--- GuessNumber.java
+++ GuessNumber.java
@@ -5,15 +5,15 @@ public class GuessNumber {
     public static void main(String[] args) {
         int random = (int) (Math.random() * 100 + 1);
         Scanner sc = new Scanner(System.in);
-        while(random != 0) {
+        System.out.print("Enter your guess (1 to 100): ");
+        int guess = sc.nextInt();
+        while(random != guess) {
             System.out.print("Enter your guess (1 to 100): ");
-            int guess = sc.nextInt();
+            guess = sc.nextInt();
             if(guess > random) {
                 System.out.println("Your guess is too big");
             } else if(guess < random) {
                 System.out.println("Your guess is too small");
-            } else {
-                 break;
             }
         } 
         System.out.println("You win!");       
