5891c2c95f2c0dffe4a33d5708702159424a82ea
sofya-S <sofyas@brainway.co.il>
2022-10-18 15:46:35 +0300

    change while to do-while

--- GuessNumber.java
+++ GuessNumber.java
@@ -5,17 +5,16 @@ public class GuessNumber {
     public static void main(String[] args) {
         int random = (int) (Math.random() * 100 + 1);
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter your guess (1 to 100): ");
-        int guess = sc.nextInt();
-        while(random != guess) {
+        int guess;
+        do {
+            System.out.print("Enter your guess (1 to 100): ");
+            guess = sc.nextInt();
             if(guess > random) {
                 System.out.println("Your guess is too big");
             } else if(guess < random) {
                 System.out.println("Your guess is too small");
             }
-            System.out.print("Enter your guess (1 to 100): ");
-            guess = sc.nextInt();            
-        } 
-        System.out.println("You win!");       
+        } while(random != guess);
+        System.out.println("You win!");     
     }
 }
