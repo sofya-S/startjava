6ef275c00403a06284af02bb752ea3127b35493f
sofya-S <sofyas@brainway.co.il>
2022-10-30 14:49:59 +0300

    Create Person.java

---
+++ Lesson_2/Person.java
@@ -0,0 +1,15 @@
+public class Person{
+    String name = "Ivan";
+    String sex = "Male";
+    int age = 27;
+    int weight = 80;
+    public void beBorn() {
+        System.out.println("you are born");
+    }
+    public void suffer() {
+        System.out.println("then you suffer");
+    }
+    public void die() {
+        System.out.println("then you die");
+    }    
+}
